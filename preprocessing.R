ccc8867039b5a2fbec30f4f0af04282d5300128f
Ruddy0365 <ruddy.nana@kaeyros-analytics.com>
2024-04-12 16:41:28 +0100

    second push

--- preprocessing.R
+++ preprocessing.R
@@ -1,46 +1,93 @@
 library(stringr)
+library(writexl)
 
-# Fonction pour extraire les informations des messages et ajouter l'ID du fichier
+
 extract_message_info <- function(path) {
   messages <- readLines(path, encoding = "latin1")  # Importer les messages depuis le fichier
 
   # Fonction pour extraire l'ID du nom de fichier
   extract_id <- function(path) {
     filename <- basename(path)  # Obtenir le nom de fichier à partir du chemin
-    id_match <- str_match(filename, "\\d{6}")  # Extraire les 6 chiffres du nom de fichier
-    id <- id_match[1, 1]  # Récupérer la première correspondance (la seule dans ce cas)
+    id_match <- str_match(filename, "(\\d{6})\\.")  # Extraire les 6 derniers chiffres du nom de fichier
+    id <- gsub("\\.", "", id_match[1] ) # Récupérer la correspondance
     return(id)
   }
 
-  # Application de la fonction à chaque message
-  message_info <- lapply(messages, function(message) {
-    phone <- str_extract(message, "\\+\\d{11}")  # Extraction du numéro de téléphone
-    datetime <- str_extract(message, "\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}")  # Extraction de la date et l'heure
-    name <- str_extract(message, "^[^(]+")  # Extraction du nom du participant
-    content_match <- str_match(message, ": (.*)")  # Extraction du contenu du message sans ":"
-    content <- content_match[, 2]  # Récupération du contenu extrait
-    return(list(name = name, phone = phone, datetime = datetime, content = content))
-  })
+  # Initialiser les variables pour stocker les informations
+  Phone <- c()
+  Datetime <- c()
+  Name <- c()
+  Content <- c()
 
-  # Création du dataframe
-  df <- data.frame(matrix(unlist(message_info), nrow = length(message_info), byrow = TRUE))
-  colnames(df) <- c("Nom", "Numéro de téléphone", "Date et heure", "Contenu du message")
+  # Parcourir chaque ligne du fichier
+  for (line in messages) {
+    # Si la ligne contient une date et une heure
+    if (grepl("\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}", line)) {
+      # Extraire les informations de la ligne
+      phone <- str_extract(line, "\\+\\d{11,}")  # Extraction du numéro de téléphone
+      datetime <- str_extract(line, "\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}")  # Extraction de la date et l'heure
+      name <- str_extract(line, "^[^(]+")  # Extraction du nom du participant
+      content_match <- str_match(line, ": (.*)")  # Extraction du contenu de la ligne sans ":"
+      content <- content_match[, 2]  # Récupération du contenu extrait
 
-  # Ajouter une colonne "id" à df
-  df$id <- extract_id(path)
+      Phone <- c(Phone, phone)
+      Datetime <- c(Datetime, datetime)
+      Name <- c(Name, name)
+      Content <- c(Content, content)
+    }
+    # Si la ligne ne contient pas une date et une heure
+    else {
+      # Concaténer le dernier element de Content avec la ligne
+      Content[length(Content)] <- paste(Content[length(Content)], line)
+    }
+  }
 
-  # Réorganiser les colonnes pour mettre "id" en première position
-  df <- df[, c("id", setdiff(colnames(df), "id"))]
+  # Supprimer les numéros de téléphone de la colonne Nom
+  Name <- gsub("\\+\\d{11,}", "", Name)
+
+  # Retourner les informations sous forme de dataframe
+  df <- data.frame(
+    id = extract_id(path),
+    `Nom agent` = Name,
+    `Numéro de téléphone` = Phone,
+    `Date et heure` = Datetime,
+    `Contenu du message` = Content,
+    stringsAsFactors = FALSE
+  )
 
   return(df)
 }
 
-# Exemple d'utilisation
+
+# Créer le répertoire de sortie s'il n'existe pas
+output_folder <- file.path(path_data, "XLSX_conversationsAFB2020_Aout")
+if (!file.exists(output_folder)) {
+  dir.create(output_folder, recursive = TRUE)
+}
+
+# Fonction pour parcourir les fichiers d'un dossier et appliquer la fonction extract_message_info
+apply_extract_message_info <- function(folder_path) {
+  # Obtenir la liste des fichiers dans le dossier
+  files <- list.files(folder_path, full.names = TRUE)
+
+  # Parcourir chaque fichier
+  for (file in files) {
+    # Appliquer la fonction extract_message_info à chaque fichier et stocker le résultat dans la liste
+    df <- extract_message_info(file)
+
+    # Construire le chemin pour sauvegarder le fichier RDS
+    output_path <- file.path(output_folder, paste0(basename(file), ".xlsx"))
+
+    # Save dataframe as a CSV file
+    write_xlsx(df, output_path)#, row.names = FALSE, fileEncoding = "latin1")
+
+  }
+}
+
+
+# utilisation
 root <- getwd()
 path_data <- file.path(root, "data")
-path_single_14182651215 <- file.path(path_data, "conversationsAFB2020_Aout", "+14182651215_2020-12-12 182825.txt")
-df <- extract_message_info(path_single_14182651215)
+folder_path <- file.path(path_data, "conversationsAFB2020_Aout")
+apply_extract_message_info(folder_path)
 
-# Save dataframe as a CSV file
-write.csv(df, file.path(path_data,"CSV_conversationsAFB2020_Aout","+14182651215_2020-12-12 182825.csv"),
-          row.names = FALSE, fileEncoding = "latin1")
